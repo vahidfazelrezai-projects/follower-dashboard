5547f701d330ba430d18f3305d7ddcb308569ee6
Vahid Fazel-Rezai <vahidfazelrezai@gmail.com>
2019-03-17 00:59:50 -0700

    query backend

---
+++ server.js
@@ -0,0 +1,26 @@
+const app = require('express')();
+const rs = require('rockset')(process.env.ROCK_API_KEY, 'https://api.rs2.usw2.rockset.com');
+
+app.get('/followers', (req, res) => {
+  rs.queries.query({
+    'sql': {
+      'query': `
+SELECT 
+    handle, 
+    ARRAY_AGG(ARRAY_CREATE(_event_time, followers)) data
+FROM yang.twitter_followers 
+WHERE _event_time > CURRENT_TIMESTAMP() - DAYS(7)
+GROUP BY handle
+`
+    }
+  }, (err, response, body) => {
+    if (err) {
+      console.log(err);
+      res.status(500).send(err);
+    }
+    console.log(response);
+    res.send(response.results);
+  });
+});
+
+app.listen(3000, () => console.log('server starting...'));

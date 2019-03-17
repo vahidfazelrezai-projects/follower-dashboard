c79381495c12f8029478f30c0a5d295bd30322fe
EC2 Default User <vahidfazelrezai@gmail.com>
2019-03-17 10:00:10 +0000

    update config

--- server.js
+++ server.js
@@ -1,24 +1,27 @@
+const fs = require('fs');
 const app = require('express')();
-const rs = require('rockset')(process.env.ROCK_API_KEY, 'https://api.rs2.usw2.rockset.com');
+const rs = require('rockset')(
+  process.env.ROCK_API_KEY || fs.readFileSync('ROCK_API_KEY', 'utf8'), 
+  'https://api.rs2.usw2.rockset.com',
+);
 
 app.get('/followers', (req, res) => {
   rs.queries.query({
     'sql': {
       'query': `
-SELECT 
-    handle, 
-    ARRAY_AGG(ARRAY_CREATE(_event_time, followers)) data
-FROM yang.twitter_followers 
-WHERE _event_time > CURRENT_TIMESTAMP() - DAYS(7)
-GROUP BY handle
-`
+        SELECT 
+          handle, 
+          ARRAY_AGG(ARRAY_CREATE(_event_time, followers)) data
+        FROM yang.twitter_followers 
+        WHERE _event_time > CURRENT_TIMESTAMP() - DAYS(7)
+        GROUP BY handle
+      `,
     }
   }, (err, response, body) => {
     if (err) {
       console.log(err);
       res.status(500).send(err);
     }
-    console.log(response);
     res.send(response.results);
   });
 });
